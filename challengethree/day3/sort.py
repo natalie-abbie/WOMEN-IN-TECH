95fa348fec0d577f51643234d0ac07b31e9dc7fa
Abio Nataline <44293940+natalie-abbie@users.noreply.github.com>
2018-11-28 11:10:09 -0500

    add the editted version of sorting list using function

--- challengethree/day3/sort.py
+++ challengethree/day3/sort.py
@@ -1,10 +1,23 @@
-#list to be sorted as even,odd and strings
-list = ([2,0,6,5,1,7,'z','a'])
-#sort int modulus 2 and returns even numbers
-intList=sorted([n for n in list if type(n) is int and n % 2 == 0])
-#sort int if not modulus 2 and return odd numbers
-intListodd=sorted([n for n in list if type(n) is int and n % 2 != 0])
-#return strings
-strList=sorted([n for n in list if type(n) is str ])
+def list_sort(n):
+   
+    intList = []
+    intListodd = []
+    strList = []
+    #sort int modulus 2 and returns even numbers
+    for a in n: 
+        #checking for even and odd numbers
+        if type(a) is int and a % 2 == 0:
+            intList.append(a)
 
-print("Even:", intList,"odd:", intListodd,"character:", strList)
+        elif type(a) is int and a % 2 != 0:
+            intListodd.append(a)
+
+        #checking for strings
+        elif type(a) is str:
+            strList.append(a)
+        
+             
+    return {"Even": intList,"odd": intListodd,"character": strList}
+
+print(list_sort([2,0,6,5,1,7,'z','a']))
+   
