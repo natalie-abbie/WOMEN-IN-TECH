bad0704c4b8514d05c777957b0cf0f4eae18baba
Abio Nataline <44293940+natalie-abbie@users.noreply.github.com>
2018-12-05 14:13:34 +0300

    fast-foods-app

--- challengefive/fastfoodapp.py
+++ challengefive/fastfoodapp.py
@@ -1,62 +1,107 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json, flash, session
 
 app = Flask(__name__)
 
-orders = [
-    {
-        'name_of_food': 'pizza',
-        'price': '30000',
-        'quantity': '1', 
-        'restuarant': 'javas'
-    },
-    {
-        'name_of_food': 'chapati',
-        'price': '500',
-        'quantity': '1', 
-        'restuarant': 'Fiaz cafe'
-    },
+# secret key to access the session of the log in 
+app.secret_key = "natalie123"
 
-    {
-        'name_of_food': 'Rolex',
-        'price': '1000',
-        'quantity': '2', 
-        'restuarant': 'Ugaroll'
-    }
-]
+orders = []
 
-users = []
+users = {}
 
-def find_user():
-  password = ['password']
-  username = ['username']
-  user = [user for user in users if user['username'] == username or user['password'] == password]
-  return user
+# routes for the api 
 
+# print("Welcome to the system. Please register or login.")
+# print("Options: register | login | exit")
+# while True:
+#     option = input("> ")
+#     if option == "login":
+#         login()
+#     elif option == "register":
+#         register()
+#     elif option == "exit":
+#         break
+#     else:
+#         print(option + " is not an option")
 
-@app.route('/api/v1/register', methods=['GET'])
+# Register
+@app.route('/api/v1/register', methods=['POST'])
 def register():
-    username = ['username']
-    password = ['password']
+    while True:
+        username = input("New username: ")
+        if not len(username) > 0:
+            print("Username can't be blank")
+            continue
+        else:
+            break
+    while True:
+        password = input("New password: ")
+        if not len(password) > 0:
+            print("Password can't be blank")
+            continue
+        else:
+            break
+    print("Creating account...")
+    users[username] = {}
+    users[username]["password"] = password
+    print("Account has been created")
 
-    user = find_user()
-    if len(user) != 0:
-      return {'Error':'Username/Email already exists'}, 409
-    if username == "":
-      return {'Error':'Please input a valid username'}, 400
-    if password == "":
-      return {'Error':'Please input a valid password'}, 400
-    new_user = {
-            'id': len(users) + 1,
-            'username': username,
-            'password': password
-            }
-    users.append(new_user)
-    return {'users': users}, 200
+# Login
+@app.route('/api/v1/login', methods=['POST'])
+def login():
+    while True:
+        username = input("Username: ")
+        if not len(username) > 0:
+            print("Username can't be blank")
+        else:
+            break
+    while True:
+        password = input("Password: ")
+        if not len(password) > 0:
+            print("Password can't be blank")
+        else:
+            break
+
+    if loginauth(username, password):
+        return session(username)
+    else:
+        print("Invalid username or password")
+
+@app.route('/api/v1/loginauth', methods=['GET'])
+# Login authorization
+def loginauth(username, password):
+    if username in users:
+        if password == users[username]["password"]:
+            print("Login successful")
+            return True
+    return False
 
 
-@app.route('/api/v1/orders', methods=['GET'])
-def get_orders():
-    return jsonify({'orders': orders})
+# @app.route('/api/v1/logout')
+# def logout():
+#     session['logged out'] = None
+#     flash('you were just logged out!')
+#     return jsonify({'message':'successful'}) 
+
+
+@app.route('/api/v1/order', methods=['POST'])
+def order():
+   global orders
+   # name_of_food, price, quantity, restaurant
+
+   name = ['name']
+   price = ['price']
+   quantity = ['quantity']
+   restaurant = ['restaurant']
+
+   orders.append({
+       'name_of_food': name,
+       'price': price,
+       'quantity': quantity,
+       'restaurant': restaurant
+   })
+
+   return json.dumps(orders)
 
 
 if __name__ == '__main__':
