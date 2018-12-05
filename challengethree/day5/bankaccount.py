29cfb4b936289ca9c9d48e8bea0ffd38d6b43234
Abio Nataline <44293940+natalie-abbie@users.noreply.github.com>
2018-12-05 16:36:28 +0300

    bankaccount

--- challengethree/day5/bankaccount.py
+++ challengethree/day5/bankaccount.py
@@ -1,10 +1,10 @@
-class BankAccount:
-    
-    def __init__(self, account_holder,password):
-         self.balance = 0
-        self.account_holder = account_holder
-        self.password = password
-        
+class BankAccount():
+    def __init__(self, account_number, name, pin_number):
+        self.status = "open"
+        self.account_number = account_number
+        self.name = name
+        self.pin_number = pin_number
+
 #user functions that the user of the account will need when accessing the account
     def user_function(self):
         print("For any action on your account: ")
@@ -16,34 +16,34 @@ class BankAccount:
              + "E: Exit service" + "\n")
 
     def get_balance(self):
-        pass
+        if self.status != "open":
+            raise ValueError
+        return self.balance
 
-    def open(self):
-        pass
+    def open(self, balance = 0):
+        self.balance = balance
+        return self.balance 
 
-#increases the balance by adding the topped up amount and returns new amount
     def deposit(self, amount):
-        self.balance = self.balance + amount
+        if self.status == "closed":
+            raise ValueError
+        if amount <=0:
+            raise ValueError
+
+        self.balance += amount
         return self.balance
 
-#decreases the amount and returns the current amount after deduction
     def withdraw(self, amount):
-        if amount > self.balance:
-            return 'sorry your account has less amount.'
-        self.balance = self.balance - amount
+        if self.balance < amount:
+            raise ValueError
+        
+        if amount < 0:
+            raise ValueError
+        
+        self.balance -= amount
         return self.balance
 
-    #To check the user password whether as stated in self.password if wrong close account
-    def password_check(self):
-        attempts = 2
-        while attempts > 0:
-            answer = input("Please type in your password to continue with the transaction:" + "\n")
-            if answer is self.password:
-                return True
-            else:
-                print ("That is the wrong password")
-                attempts -= 1
-                print ("{attempts} more attempt(s) remaining".format(attempts=attempts))
-
-        print ("Sorry your account is closed due to wrong password attemps.")
-        exit()
+    def close(self):
+        #close account 
+        self.status = "closed"
+        return self.status
