3158630eea3245aa7cd930584e4178eed87180c8
Abio Nataline <44293940+natalie-abbie@users.noreply.github.com>
2018-12-08 10:41:16 -0500

    add these changes to the flask api

--- challengefive/fastfoodapp.py
+++ challengefive/fastfoodapp.py
@@ -6,7 +6,10 @@ orders = []
 
 users_list = []
 
-# routes for the api 
+loggedinuser = []
+
+# routes for the api
+
 
 @app.route('/api/v1/register', methods=['POST'])
 def register():
@@ -14,69 +17,106 @@ def register():
 
     username = data['username']
     password = data['password']
+    role = data['role']
 
-    users= {
-        'username':username,
-        'password':password
-        }
+    try:
+        username = data['username']
+        password = data['password']
+        role = data['role']
+
+    except KeyError as item:
+        return jsonify({'message': str(item)+'missing'}), 400
+
+    users = {
+        'username': username,
+        'password': password,
+        'role': role
+    }
     users_list.append(users)
 
-    if not len(username) > 0:
-        return jsonify({"message":"Username can't be blank"}), 400
-    
-    if not len(password) > 0:
-        return jsonify({"message":"Password can't be blank"}), 400
+    for users in users_list:
+
+        if not len(username) > 0:
+            return jsonify({"message": "username can't be blank"}), 400
+
+        if not len(password) > 0:
+            return jsonify({"message": "password can't be blank"}), 400
         
-    else:
-        return jsonify({"message":"Account created successfully",'users': users_list}), 200
+        if not len(role) > 0:
+            return jsonify({"message": "role can't be blank"}), 400
+
+        else:
+            return jsonify({"message": "Account created successfully", 'users': users_list}), 200
 
 # Login
 @app.route('/api/v1/login', methods=['POST'])
 def login():
 
-        data = request.get_json()
+    data = request.get_json()
 
+    username = data['username']
+    password = data['password']
+    role = data['role']
+
+    try:
         username = data['username']
         password = data['password']
+        role = data['role']
 
-        if not len(username) > 0 and not len(password) > 0:
-            return jsonify({"Error": "field can't be blank"}), 400
+    except KeyError as item:
+        return jsonify({'message': str(item)+'missing'}), 400
+
+    users = {
+        'username': username,
+        'password': password,
+        'role': role
+    }
+
+    for users in users_list:
+
+        if not len(username) > 0 and not len(password) > 0 and not len(role) > 0:
+            return jsonify({"message":"fields can't be blank"}), 400
+
+        if username == username and password == password and role == role:
+            return jsonify({"message": "login successful"}), 200
         
-        if password == users_list[0]["password"]:
-            return jsonify({"message":"Login successful"}), 200
+        if username != username and password != password and role != role:
+                return jsonify({"message": "invalid"})
 
         else:
-            return jsonify({"message":"invalid"})        
+           return jsonify({"message":"invalid"})
+            
 
-     
+
 @app.route('/api/v1/session', methods=['POST'])
 def session():
-    
-    username=[]
+
+    username = []
     optiondata = request.get_json()
 
     option = optiondata["your option"]
     if option == "logout":
         return jsonify({"message": "you are logged out"}), 200
-        
+
     elif option == "view orders":
         return order()
+
 
 @app.route('/api/v1/order', methods=['POST'])
 def order():
    global orders
    # name_of_food, price, quantity, restaurant
-   userdata=request.get_json()
+   userdata = request.get_json()
    try:
         name = userdata['name']
         price = userdata['price']
         quantity = userdata['quantity']
         location = userdata['location']
-       
-   except KeyError as item:
-       return jsonify({'message':str(item)+'missing'}),400
 
-   new_order={
+   except KeyError as item:
+       return jsonify({'message': str(item)+'missing'}), 400
+
+   new_order = {
        'name_of_food': name,
        'price': price,
        'quantity': quantity,
@@ -84,11 +124,13 @@ def order():
    }
 
    orders.append(new_order)
-   return jsonify({"message":"order created"}),201
+   return jsonify({"message": "order created"}), 201
+
 
 @app.route('/api/v1/order', methods=['GET'])
 def get_order():
-   return jsonify({'orders':orders}),200
+   return jsonify({'orders': orders}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
